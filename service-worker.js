a43fb3fdd1f7c019937001083d03b3e27daefffa
2022339021-cpu <2022339021@student.sust.edu>
2026-07-22 01:20:41 +0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "tuition-ledger-v1";
+
+const urlsToCache = [
+  "/Tution/",
+  "/Tution/index.html",
+  "/Tution/manifest.json",
+  "/Tution/icon-192.png",
+  "/Tution/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
